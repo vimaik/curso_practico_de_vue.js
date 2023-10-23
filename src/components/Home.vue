d9ea7bd6d342dbc877619d88c6dd3ad5f39e6637
vimaik <victormg2k@gmail.com>
2023-10-23 15:24:50 +0200

    04. Header y contenido (Uso de slots)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Componente 'Home'</div>
+  <Layout>
+    <template #header>
+      <Header />
+    </template>
+  </Layout>
 </template>
+
+<script>
+import Layout from "@/components/Layout.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Layout,
+    Header,
+  }
+};
+</script>
