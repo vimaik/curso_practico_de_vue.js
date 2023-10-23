646479901ae41d0534913eb6c09b0bc6fd7d2df5
vimaik <victormg2k@gmail.com>
2023-10-23 15:25:36 +0200

    05. Layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,27 @@
     <template #header>
       <Header />
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </Layout>
 </template>
 
 <script>
 import Layout from "@/components/Layout.vue";
 import Header from "@/components/Header.vue";
+import Resume from "@/components/Resume/Index.vue";
+import Movements from "@/components/Movements.vue";
 
 export default {
   components: {
     Layout,
     Header,
+    Resume,
+    Movements,
   }
 };
 </script>
